3fa1de13026f271ddf4f17d6d140002660515257
Andrew Robinette <ajrobine@unca.edu>
2012-02-08 11:28:34 -0500

    first commit

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -3,19 +3,67 @@
  */
 function main() {
     //your code goes here
-    alert("hello world!");
+    //alert("hello world!");
+
+    var count = 0;
+    var object_array = [];
 
 
     //your tasks
 
     //1. Create a spotter and get it to insert tweets into the DOM
-    //2. Add profile images (tweet.profile_image_url)
+
+    var s = new Spotter("twitter.search",
+    			{q:"bieber", period:120},
+			{buffer:true, bufferTimeout:750}
+			);
+
+        
+
+    s.register(function(tweet) {
+        
+	//2.  Add profile images (tweet.profile_image_url)
+
+	count = count + 1;
+	var color;
+
+	var profile_image = "<img src='"+tweet.profile_image_url+"' />";
+
+	if (count%2 === 0) {
+	   color = "red";
+	}  else  {
+	     color = "blue";
+	}
+
+	var object = $("<p class='"+color+"'>"+profile_image+tweet.text+"</p>");
+	object.hide();
+	$("#tweets").prepend(object);
+	object.slideDown();
+	
+	object_array.push(object);
+
+	if(object_array.length > 4) {
+	  var object_to_remove = object_array.shift();
+	  object_to_remove.fadeOut(200, function() {
+	     object_to_remove.remove();
+	     });
+	  }
+	});
+
+    s.start();
+
+    
+
+    
+
     //3. Make the tweets occur so the most recent are at the top
     //4. Make the tweets slide down
     //5. Alternate the colors or the background of the tweets
+
+
     //6. Show a maximum of 10 tweets at a time (remove old tweets from the dom)
 
-
+	
 }
 
 main();
